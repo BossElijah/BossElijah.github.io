73fdf2b7e1b40c426e4f2de68bd140a988f504c3
Elias Bruhn <elias@lfpost.dk>
2022-04-30 00:12:27 +0200

    Use different logo on mobile than on desktop

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,27 @@
 import PropTypes from 'prop-types';
 import { Link, NavLink } from 'react-router-dom';
 
-const Header = ({ logo, links }) => (
+const Header = ({ logoMobile, logoDesktop, links }) => (
   <header>
     <Link to="/">
-      <img src={logo.src} alt={logo?.alt} className="header__logo" />
+      <img
+        src={logoMobile.src}
+        alt={logoMobile?.alt}
+        className="header__logo mobile"
+      />
+      <img
+        src={logoDesktop.src}
+        alt={logoDesktop?.alt}
+        className="header__logo desktop"
+      />
     </Link>
     <ul className="nav-links">
       {links.map(({ url, label }) => (
         <li key={url}>
-          <NavLink to={url} activeClassName="active">
+          <NavLink
+            to={url}
+            className={({ isActive }) => (isActive ? 'active' : '')}
+          >
             {label}
           </NavLink>
         </li>
@@ -19,7 +31,11 @@ const Header = ({ logo, links }) => (
 );
 
 Header.propTypes = {
-  logo: PropTypes.shape({
+  logoMobile: PropTypes.shape({
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+  }).isRequired,
+  logoDesktop: PropTypes.shape({
     src: PropTypes.string.isRequired,
     alt: PropTypes.string.isRequired
   }).isRequired,
